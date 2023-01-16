b2dacde9d28758a4dd3eb753204c363464063bb2
thomas <thomas@PEAXdata.com>
2023-01-16 18:33:20 +0100

    WIP

---
+++ scrape.py
@@ -0,0 +1,27 @@
+# this script deletes the Offers from database and runs the scrapy scraper
+# to fill it again
+import os
+import django
+
+from scraper.discog.spiders.marketplace import MarketPlaceSpider
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'backend.backend.settings'
+django.setup()
+from django.contrib.auth.models import User
+from api.models import Offer
+
+os.environ['SCRAPY_SETTINGS_MODULE'] = 'scraper.discog.settings'
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+
+
+USER_ID = 2
+user = User.objects.get(id=USER_ID)
+Offer.objects.filter(user=user).delete()
+
+
+process = CrawlerProcess(get_project_settings())
+process.crawl(MarketPlaceSpider)
+process.start()
+
