4ad007a6b0ff9280877f9026b0707eab0b244e3a
thomas <thomas@PEAXdata.com>
2023-02-03 13:34:25 +0100

    wip deleting large parts of the code

--- api/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Master, Offer
-
-admin.site.register(Master)
-admin.site.register(Offer)
--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('wants', views.WantsView.as_view()),
-    path('want/<int:pk>', views.WantDelete.as_view()),
-    path('add-master/<int:master_id>', views.AddMaster.as_view())
-    ]
